6d8ae09bd5ec0bc6ef91a5718210b01826c5a817
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-12 23:56:38 -0400

    first half of the challenges

---
+++ script.js
@@ -0,0 +1,81 @@
+// Exercise One //
+
+	// Part A //
+
+	// var one = 1;
+	// var two = 2;
+
+	// console.log (one + two);
+
+	// Part B //
+
+	// var one = "1";
+	// var two = "2";
+
+	// console.log (one + two);
+
+// Exercise Two //
+
+	// var india = ["Bangalore", "Alapuzzha", "Chennai"];
+	// var us = ["San Francisco", "Denver", "Philadelphia"];
+	// var germany = ["Hamburg", "Berlin", "Dusseldorf"];
+	// var favPlaces = [india, us, germany];
+	// var itinerary = favPlaces[0][1];
+
+	// console.log("Let's take a trip to " + itinerary);
+
+// Exercise Three //
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+// 	function guessNumber() {
+// 		var number = document.getElementById("number").value;
+// 		var button = document.getElementById("button");
+
+// 		if (number < 100) {
+// 			alert(number + " is less than 100")
+// 		}
+// 		else if (number > 100) {
+// 			alert(number + " is greater than 100")
+// 		}
+// 	}; // ends guessNumber();
+
+// 	button.addEventListener("click", guessNumber);
+
+// }); // ends the ready function
+
+// Exercise Five //
+
+	// document.addEventListener("DOMContentLoaded", function(event) {
+	// 	function truthTeller() {
+	// 		var wordOne = document.getElementById("word1").value;
+	// 		var wordTwo = document.getElementById("word2").value;
+	// 		var button = document.getElementById("button");
+
+	// 		if (wordOne == wordTwo) {
+	// 			alert("We have a match!")
+	// 		}
+	// 		else {
+	// 			alert("This is Disgraceful!")
+	// 		};
+
+	// 	}; // ends truthTeller();
+
+	// 	button.addEventListener("click", truthTeller);
+
+	// }); // ends the ready function
+
+// Exercise Six //
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
